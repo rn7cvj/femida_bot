112c441838263aee5bfbe6f40b54de9247c9ee5e
rn7cvj <rn7cvj@outlook.com>
2025-11-27 19:11:45 +1000

    update restart

--- restart.sh
+++ restart.sh
@@ -3,7 +3,6 @@
 CONTAINER_NAME="femida_bot"
 IMAGE_NAME="femida_bot"
 
-# Проверяем, существует ли контейнер
 if [ "$(docker ps -aq -f name=^/${CONTAINER_NAME}$)" ]; then
     echo "Остановка и удаление контейнера: $CONTAINER_NAME"
     docker stop $CONTAINER_NAME
@@ -12,7 +11,6 @@ else
     echo "Контейнер $CONTAINER_NAME не найден."
 fi
 
-# Проверяем, существует ли образ, и удаляем его
 if [ "$(docker images -q $IMAGE_NAME)" ]; then
     echo "Удаление образа: $IMAGE_NAME"
     docker rmi $IMAGE_NAME
@@ -20,16 +18,9 @@ else
     echo "Образ $IMAGE_NAME не найден."
 fi
 
-# Сборка нового образа
 echo "Сборка образа: $IMAGE_NAME"
 docker build -t $IMAGE_NAME .
 
-# Запуск контейнера
+
 echo "Запуск контейнера: $CONTAINER_NAME"
-# Используем --env-file .env, если файл существует, иначе просто запускаем
-if [ -f .env ]; then
-    docker run -d --name $CONTAINER_NAME --env-file .env $IMAGE_NAME
-else
-    echo "Файл .env не найден, запускаем без него (убедитесь, что переменные окружения заданы)"
-    docker run -d --name $CONTAINER_NAME $IMAGE_NAME
-fi
+docker run -d --name $CONTAINER_NAME $IMAGE_NAME
